e022c349eb5ae499861f785056519b38fb97c6b9
Singareddy <nsingareddy@ptc.com>
2018-06-11 12:17:49 +0530

    updated the CalcTest.java

--- src/test/java/demo/CalcTest.java
+++ src/test/java/demo/CalcTest.java
@@ -1,11 +1,32 @@
 package demo;
 
 import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalcTest {
  private Calc c1;
-	
+ @BeforeClass
+ public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		c1 = new Calc();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+ 
  	@Test
 	public void testAdd() {
 		int i = c1.add(10,20);
